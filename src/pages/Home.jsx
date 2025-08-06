5ceaf15b252d4d680fac1a7f189f1863e667faa1
Muluken Seifu <muleseifu87@gmail.com>
2025-08-06 15:49:01 +0300

    Add some functionalities to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
 import MovieCard from "../components/movie-card.jsx";
+import { useState } from "react";
 
 function Home() {
+    //This is useful for managing the state of the search query, and using onChange inside the input field to update the state as the user types.
+
+    //The useState hook is used to create a state variable called searchQuery, which is initialized to an empty string. The setSearchQuery function is used to update the value of searchQuery.
+
+
+    //used for real time search functionality, allowing the user to type in a search term and see results immediately.
+    
+    const [searchQuery, setSearchQuery] = useState("");
+    
     const movies = [
         { id: 1, Title: "Inception", Year: "2010", Poster: "https://example.com/inception.jpg" },
         { id: 2, Title: "The Matrix", Year: "1999", Poster: "https://  example.com/matrix.jpg" },
@@ -10,8 +20,10 @@ function Home() {
         { id: 6, Title: "Forrest Gump", Year: "1994", Poster: "https://example.com/forrestgump.jpg" }]
 
     // Function to handle search submission
-    
-        const handleSearchSubmit = () => {
+
+        const handleSearchSubmit = (e) => {
+            e.preventDefault(); // Prevent the default form submission behavior, which would refresh the page.
+            alert(`Searching for: ${searchQuery}`);
         
         }
 
@@ -21,12 +33,18 @@ function Home() {
   return (
     <div className="home">
         <form onSubmit={handleSearchSubmit} className="search-form">
-            <input type="text" placeholder="Search for a movie..." className="search-input"/>
-            <button type="submit">Search</button>
+            <input type="text" placeholder="Search for a movie..." className="search-input" value={searchQuery} 
+            onChange={(e) => setSearchQuery(e.target.value)}/> 
+            
+            <button type="submit" className="search-button">Search</button> 
         </form>
       <div class name="movie-grid"> 
         {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} /> //key is essential for React to identify which items have changed, are added, or are removed even if we did not use it in the previous code.
+            movie.Title.toLowerCase().startsWith(searchQuery.toLowerCase()) && // Filter movies based on search query
+          <MovieCard key={movie.id} movie={movie} />
+          
+          
+          //key is essential for React to identify which items have changed, are added, or are removed even if we did not use it in the previous code.
         ))}
       </div>
     </div>
