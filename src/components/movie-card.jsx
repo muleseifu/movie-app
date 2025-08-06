7dfc0d0067be92f87500b66a502c9f0381aba862
Muluken Seifu <muleseifu87@gmail.com>
2025-08-06 17:27:15 +0300

    Add image path correctly

--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -12,7 +12,7 @@ function MovieCard({movie}) {
     return (
         <div className="movie-card">
             <div className="movie-poster">
-                <img src={movie.Poster} alt={movie.title} />
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
             </div>
             <div className="movie-overlay">
                 <button className="favorite-btn" onClick={handleFavoriteClick}> 
