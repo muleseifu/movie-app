a54154cbb9f062d8ea0d78f3ceb69bd7421067b6
Muluken Seifu <muleseifu87@gmail.com>
2025-08-07 14:14:35 +0300

    make favorites page work

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,24 @@
 import "../css/Favorites.css";
-import { useMovieContext,  } from "../contexts/MovieContext";
+import { useMovieContext  } from "../contexts/MovieContext";
 import MovieCard from "../components/movie-card";
 
 
 function Favorites() {
     const { favorites } = useMovieContext();
     if (favorites) {
-        return
+        return (
+            <div className="favorites">
+            <h2>Your Favorites</h2>
+            <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+
+
+            //key is essential for React to identify which items have changed, are added, or are removed even if we did not use it in the previous code.
+          ))}
+        </div>
+        </div>
+        )
     }
     return (
         <div className="favorites-empty">
