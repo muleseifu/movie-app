8102b298e4285a755c1c38041e9cdef61c6450f6
Muluken Seifu <muleseifu87@gmail.com>
2025-08-07 14:20:57 +0300

    make navbar fixed

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to="/">Movie App</Link>
+                <Link to="/">GetMovies</Link>
             </div>
             <div className="navbar-links">
                 <Link to="/" className="nav-link">Home</Link>
